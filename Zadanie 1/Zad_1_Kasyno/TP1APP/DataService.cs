7aabb1898a8a150d3a0a766f791ffa2746d0a835
KrzysztofKosmala <37869082+KrzysztofKosmala@users.noreply.github.com>
2019-05-12 23:03:33 +0200

    Update dataservice

--- Zadanie 1/Zad_1_Kasyno/TP1APP/DataService.cs
+++ Zadanie 1/Zad_1_Kasyno/TP1APP/DataService.cs
@@ -80,6 +80,140 @@ namespace TP1APP
         }
         #endregion
 
-   
+        #region Zdarzenia
+
+        public Zdarzenie DodajZdarzenie(int id, OpisStanu opisStanu, Wykaz wykaz, DateTime dataGry)
+        {
+            Zdarzenie z = new Zdarzenie(id, opisStanu, wykaz, dataGry);
+            dataRepository.DodajPartie(z);
+            return z;
+        }
+
+        public void PobierzZdarzeniePoId(int id)
+        {
+            dataRepository.PobierzPartiePoId(id);
+        }
+
+        public void UaktualnijZdarzeniePoId(int id, int noweId, OpisStanu opisStanu, Wykaz wykaz, double ilosc, DateTime dataGry)
+        {
+            Zdarzenie z = DodajZdarzenie(noweId, opisStanu, wykaz, dataGry);
+            dataRepository.UaktualnijPartiePoId(id, z);
+        }
+        public void UsunZdarzeniePoId(int id)
+        {
+            dataRepository.UsunPartiePoId(id);
+        }
+        #endregion
+
+        #region Gry
+
+        public Katalog DodajGre(int id, string nazwa, int iloscGraczy, double stawka)
+        {
+            Katalog k = new Katalog(id, nazwa, iloscGraczy, stawka);
+            dataRepository.DodajGre(k);
+            return k;
+        }
+
+        public void PobierzGrePoId(int id)
+        {
+            dataRepository.PobierzGrePoId(id);
+        }
+
+        public void UaktualnijGrePoId(int id, int noweId, string nazwa, int iloscGraczy, double stawka)
+        {
+            Katalog k = DodajGre(noweId, nazwa, iloscGraczy, stawka);
+            dataRepository.UaktualnijGrePoId(id, k);
+        }
+
+        public void UsunKsiazkePoId(int id)
+        {
+            dataRepository.UsunGrePoId(id);
+        }
+        #endregion
+
+        #region Gracz
+        public Wykaz DodajGracza(int id, string imie, string nazwisko, string telefon, string adres)
+        {
+            Wykaz cz = new Wykaz(id, imie, nazwisko, telefon, adres);
+            dataRepository.DodajGracza(cz);
+            return cz;
+
+        }
+
+        public void PobierzGraczaPoId(int id)
+        {
+            dataRepository.PobierzGraczaPoId(id);
+        }
+
+        public void UaktualnijGraczaNaId(int id, int noweId, string imie, string nazwisko, string telefon, string adres)
+        {
+            Wykaz gr = DodajGracza(noweId, imie, nazwisko, telefon, adres);
+            dataRepository.UaktualnijGraczaPoId(id, gr);
+        }
+
+        public void UsunGraczaPoID(int id)
+        {
+            dataRepository.UsunGraczaPoId(id);
+        }
+        #endregion
+
+        #region Wyszukiwanie danych
+
+        public void WyszukajPoImieniu(string name)
+        {
+            foreach (Wykaz w in dataRepository.PobierzWszystkichGraczy())
+            {
+                if (name == w.Imie)
+                {
+                    Console.WriteLine(w.ToString());
+                }
+            }
+        }
+
+        public void WyszukajPoNazwisku(string name)
+        {
+            foreach (Wykaz w in dataRepository.PobierzWszystkichGraczy())
+            {
+                if (name == w.Nazwisko)
+                {
+                    Console.WriteLine(w.ToString());
+                }
+            }
+        }
+
+        public void WyszukajPoTelefonie(string name)
+        {
+            foreach (Wykaz w in dataRepository.PobierzWszystkichGraczy())
+            {
+                if (name == w.Telefon)
+                {
+                    Console.WriteLine(w.ToString());
+                }
+            }
+        }
+
+        public void WyszukajPoAdresie(string name)
+        {
+            foreach (Wykaz w in dataRepository.PobierzWszystkichGraczy())
+            {
+                if (name == w.Adres)
+                {
+                    Console.WriteLine(w.ToString());
+                }
+            }
+        }
+
+        public void WyszukajPoNazwie(string name)
+        {
+            foreach (Katalog k in dataRepository.PobierzWszystkieGry().Values)
+            {
+                if (name == k.Nazwa)
+                {
+                    Console.WriteLine(k.ToString());
+                }
+            }
+        }
+
+        #endregion
     }
 }
